1a7aab15152e9bd3973a4460c749df53f58e63e8
Isa Khan <isakhan1000@gmail.com>
2024-09-25 20:12:59 -0400

    TT

--- HandDetection.py
+++ HandDetection.py
@@ -1,23 +1,22 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
-# Initialize the webcam
 cap = cv2.VideoCapture(1)
-cap.set(3, 1280)  # Set width
-cap.set(4, 720)   # Set height
+cap.set(3, 1280)  
+cap.set(4, 720)   
 
-# Initialize the hand detector
-detector = HandDetector(detectionCon=1, maxHands=2) # was 0.6
+
+detector = HandDetector(detectionCon=1, maxHands=2) 
 
 while True:
-    success, img = cap.read()  # Capture frames from the webcam
+    success, img = cap.read()  
     if not success:
         break
 
-    img = detector.findHands(img)  # Detect hands in the frame
-    cv2.imshow("Smart Camera", img)  # Display the frame with hand detection
+    img = detector.findHands(img)  
+    cv2.imshow("Smart Camera", img)  
 
-    if cv2.waitKey(1) == ord("q"):  # Exit if 'q' is pressed
+    if cv2.waitKey(1) == ord("q"):  
         break
 
 cap.release()
